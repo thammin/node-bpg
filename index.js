5c68d6bd900162ae65fd12b54e17ee163706049a
thammin <thammin@live.co.uk>
2014-12-22 02:35:33 +0900

    add basic wrapper for bpg encoder

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+'use strict';
+var spawn = require('child_process').spawn;
+
 module.exports = function() {
-  
+
+  // TODO: initialize with options
+  function getArgs(filePath) {
+    return ['-o', filePath.replace(/\.(png|jpg|jpeg)$/i, '.bpg')];
+  }
+
+  function encode(inputFilePath, outputFilePath, callback) {
+    var args = [];
+
+    if (!inputFilePath) {
+      callback(new Error('Invalid input filepath'));
+    } else {
+      args.push(inputFilePath);
+    }
+
+    if (!outputFilePath) {
+      Array.prototype.push.apply(args, getArgs(inputFilePath));
+    } else {
+      Array.prototype.push.apply(args, getArgs(outputFilePath));
+    }
+
+    var child = spawn('./libbpg/bpgenc', args);
+    child.on('exit', function(code) {
+      if (code !== 0) {
+        callback(new Error('Process bpgenc exited with non-zero code'));
+      } else {
+        callback();
+      }
+    });
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+  }
+
+  return {
+    encode: encode,
+    decode: null
+  };
 };
--- test/bpgSpec.js
+++ test/bpgSpec.js
@@ -1,9 +1,42 @@
+var bpg = require('../index.js')();
+var glob = require('glob');
+var async = require('neo-async');
+var path = require('path');
+
 describe('bpg encoder', function() {
   it('generate bpg image from JPEG', function() {
-    
+    var outputPath = 'test/fixture/output/';
+    glob('test/fixture/input/*.jpg', {}, function(err, files) {
+      if (err) throw err;
+
+      async.eachLimit(files, 3, function(file, callback) {
+        var basename = path.basename(file);
+        bpg.encode(file, outputPath + basename, function(err) {
+          if (err) throw err;
+          callback();
+        });
+      }, function(err) {
+        if (err) throw err;
+        console.log('All files have been processed successfully');
+      });
+    });
   });
-  
+
   it('generate bpg image from PNG', function() {
-    
-  })
+    var outputPath = 'test/fixture/output/';
+    glob('test/fixture/input/*.png', {}, function(err, files) {
+      if (err) throw err;
+
+      async.eachLimit(files, 3, function(file, callback) {
+        var basename = path.basename(file);
+        bpg.encode(file, outputPath + basename, function(err) {
+          if (err) throw err;
+          callback();
+        });
+      }, function(err) {
+        if (err) throw err;
+        console.log('All files have been processed successfully');
+      });
+    });
+  });
 });
